27b40ae1abcab7f636692a4ed8331baaf72adce3
ultrasue <nastya.murlla@gmail.com>
2023-01-10 19:33:36 +0300

    last pages

--- web/main/views.py
+++ web/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 
+
 def FirstPage(request):
     return render(request, 'main/index.html')
 
@@ -9,4 +10,16 @@ def Quantity(request):
 
 def Point(request):
     return render(request, 'main/3page.html')
+
+def Share(request):
+    return render(request, 'main/4page.html')
+
+def LastAwesome(request):
+    return render(request, 'main/last_page.html')
+
+def LastGood(request):
+    return render(request, 'main/last_page_2.html')
+
+def LastBad(request):
+    return render(request, 'main/last_page_3.html')
 
