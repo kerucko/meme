f9abfd2c3edd9bb860f9a22da0ddac7d86f499f5
ultrasue <nastya.murlla@gmail.com>
2023-01-10 15:33:59 +0300

    buttons

--- web/main/static/main/js/quiz.js
+++ web/main/static/main/js/quiz.js
@@ -4,9 +4,33 @@ var imgs=[
 		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdPEJ0n_tXI0No_5AgkeVpzomxp4mI8jlQv3rsKQQHNh671QU_'
 	],
 	image=document.getElementById('image'),
-	button=document.getElementById('button');
+	button_3=document.getElementById('button_3');
+	button_2=document.getElementById('button_2');
+	button_1=document.getElementById('button_1');
+	button0=document.getElementById('button0');
+	button1=document.getElementById('button1');
+	button2=document.getElementById('button2');
+	button3=document.getElementById('button3');
 
-button.onclick=function(){
+button_3.onclick=function(){
+image.src=imgs[Math.floor(Math.random()*imgs.length)];
+}
+button_2.onclick=function(){
+image.src=imgs[Math.floor(Math.random()*imgs.length)];
+}
+button_1.onclick=function(){
+image.src=imgs[Math.floor(Math.random()*imgs.length)];
+}
+button0.onclick=function(){
+image.src=imgs[Math.floor(Math.random()*imgs.length)];
+}
+button1.onclick=function(){
+image.src=imgs[Math.floor(Math.random()*imgs.length)];
+}
+button2.onclick=function(){
+image.src=imgs[Math.floor(Math.random()*imgs.length)];
+}
+button3.onclick=function(){
 image.src=imgs[Math.floor(Math.random()*imgs.length)];
 }
 
