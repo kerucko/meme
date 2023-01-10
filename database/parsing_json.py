67acb61243ad595b1ae83f1f081882ef9d980007
kerucko <cykinpavel@gmail.com>
2023-01-10 17:22:33 +0300

    edit parsing

--- database/parsing_json.py
+++ database/parsing_json.py
@@ -1,5 +1,6 @@
 import json
 import codecs
+import os
 
 path_to_json = "result.json"
 
@@ -11,6 +12,8 @@ with codecs.open(path_to_json, "r", "utf_8_sig") as read_file:
 dictdata["messages"].pop(0)
 dictdata["messages"].pop(0)
 
+photo_id = 1
+
 for d in dictdata["messages"]:
     d.pop('type')
     d.pop('date')
@@ -21,5 +24,13 @@ for d in dictdata["messages"]:
     d.pop('from_id')
     d.pop('text_entities')
 
+    photo_src = d['photo']
+    d['id'] = photo_id
+    new_photo_src = f"photos/photo_{photo_id}.jpg"
+    os.rename(photo_src, new_photo_src)
+    d['photo'] = new_photo_src
+    photo_id += 1
+
+
 with codecs.open(path_to_new_json, "w", "utf_8_sig") as write_file:
-    write_file.write(json.dumps(dictdata, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': ')))
+    write_file.write(json.dumps(dictdata["messages"], sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': ')))
