8d0ad55dc45c04ddeb77cf1e3355525a7de7e69d
sonikxx <shevlyakovasofia@icloud.com>
2023-01-11 22:47:18 +0500

    settings link

---
+++ web/main/static/main/js/link.js
@@ -0,0 +1,11 @@
+function Copy(containerid) {
+      let textarea = document.createElement('textarea');
+      textarea.id = 'temp';
+      textarea.style.height = 0;
+      document.body.appendChild(textarea);
+      textarea.value = document.getElementById(containerid).innerText;
+      let selector = document.querySelector('#temp');
+      selector.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+}
