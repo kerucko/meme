517aa9f6b3ff3e93c8a4ba6ff5f4b2db3b9f27d1
kerucko <cykinpavel@gmail.com>
2023-01-11 11:53:04 +0300

    add count_click in url

--- web/main/static/main/js/quiz.js
+++ web/main/static/main/js/quiz.js
@@ -1,22 +1,40 @@
-    image = document.getElementById('image'),
-	button_3 = document.getElementById('button_3');
-	button_2 = document.getElementById('button_2');
-	button_1 = document.getElementById('button_1');
-	button0 = document.getElementById('button0');
-	button1 = document.getElementById('button1');
-	button2 = document.getElementById('button2');
-	button3 = document.getElementById('button3');
+image = document.getElementById('image'),
+button_3 = document.getElementById('button_3');
+button_2 = document.getElementById('button_2');
+button_1 = document.getElementById('button_1');
+button0 = document.getElementById('button0');
+button1 = document.getElementById('button1');
+button2 = document.getElementById('button2');
+button3 = document.getElementById('button3');
+count = document.getElementById('Count');
 
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
 
 var count_click = 0;
-var id = "Count";
-var max = document.getElementById(id);
+var max = getUrlParameter('count_click');
 alert(max);
+
+count.insertAdjacentHTML('afterbegin', count_click + '/' + max);
 
 button_3.onclick = function(){
     if (count_click <= 5) {
         image.src="static/main/img//photos/photo_" + id_pictures + ".jpg";
         count_click = count_click + 1;
+        count.insertAdjacentHTML('afterbegin', count_click + '/' + max);
         id_pictures = Math.floor(Math.random() * 2631 + 1);
     }
     else {
@@ -29,6 +47,7 @@ button_2.onclick = function(){
     if (count_click <= 5) {
         image.src="static/main/img//photos/photo_" + id_pictures + ".jpg";
         count_click = count_click + 1;
+        count.insertAdjacentHTML('afterbegin', count_click + '/' + max);
         id_pictures = Math.floor(Math.random() * 2631 + 1);
     }
     else {
