e9f8b4f1e17735973a89951f593af15d6abb2726
kerucko <cykinpavel@gmail.com>
2023-01-12 23:48:18 +0300

    add database

--- web/main/models.py
+++ web/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Marks(models.Model):
+    marks = models.CharField('Marks', max_length=200)
+    memes = models.CharField('Memes', max_length=250)
+    unique_id = models.CharField('ID', max_length=10)
+
+    def __str__(self):
+        return self.unique_id
+
+    class Meta:
+        verbose_name = 'Результат оценивания'
+        verbose_name_plural = 'Результаты оценивания'
