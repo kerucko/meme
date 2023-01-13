b5a539df1cb7f3e2d17e8c64fb505eb9fda58b2a
kerucko <cykinpavel@gmail.com>
2023-01-13 16:18:06 +0300

    commit

--- web/main/static/main/js/link.js
+++ web/main/static/main/js/link.js
@@ -10,6 +10,18 @@ function Copy(containerid) {
       document.body.removeChild(textarea);
 }
 
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+
+alert(makeid(10));
 link = document.getElementById('toCopy');
 var memes = localStorage.getItem('memes');
 var marks = localStorage.getItem('marks');
