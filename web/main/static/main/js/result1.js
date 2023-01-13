b24f9e1e89e8392c941fbc9556f213a1ce30c0b6
kerucko <cykinpavel@gmail.com>
2023-01-13 23:16:44 +0300

    fix share button

--- web/main/static/main/js/result1.js
+++ web/main/static/main/js/result1.js
@@ -1,3 +1,41 @@
 result = document.getElementById('Result');
+title = document.getElementById('Title');
+text = document.getElementById('Text');
+
+
 var q = localStorage.getItem('result');
 result.innerHTML = '<b>' + q + '%</b>';
+
+if (q >= 80) {
+    title.innerHTML = `
+    <p>Поздравляем!</p>
+    <p>Ваш результат:</p>
+    `
+    text.innerHTML = `
+    <p>It\`s a match!</p> <p>Вы идеально подходите друг другу &ltЗ </p>
+    `
+}
+if (40 <= q && q < 80) {
+    title.innerHTML = `
+    <p>Ну...</p>
+    <p>Ваш результат:</p>
+    `
+    text.innerHTML = `
+    <p>В принципе, Вы общаетесь не с самым</p> <p>кринжовым человеком. Ура!</p>
+    `
+}
+if (q < 40) {
+    title.innerHTML = `
+    <p>Увы...</p>
+    <p>Ваш результат:</p>
+    `
+    text.innerHTML = `
+    <p>У нашей команды нет слов.... </p>
+    <p>Cовет от Кошки: прекратите общение </p>
+    <p>с этим человеком как можно скорее, мяу (шутка) </p>
+    `
+}
+
+function Repeat() {
+    document.location = "http://127.0.0.1:8000/";
+}
